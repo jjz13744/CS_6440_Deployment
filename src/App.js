1ae4fb920a69127e99bf7142bd894f283cbd3367
Jared Zengo <jjz13744@gmail.com>
2024-11-10 15:21:22 -0500

    Updating for github pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import PatientDashboard from './components/PatientDashboard';
 
 function App() {
   return (
-    <Router basename="/CS_6440_Deployment">
+    <Router>
       <div className="App">
         <Switch>
           <Route path="/patient-dashboard/:firstName/:lastName" component={PatientDashboard} />
