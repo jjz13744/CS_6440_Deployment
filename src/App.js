ffc2f3edeab08aba66f8de8f733c95f72ec6d76c
Jared Zengo <jjz13744@gmail.com>
2024-11-10 15:25:54 -0500

    Updating react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import PatientDashboard from './components/PatientDashboard';
 
 function App() {
@@ -7,6 +7,9 @@ function App() {
     <Router>
       <div className="App">
         <Switch>
+          <Route exact path="/" render={() => (
+            <Redirect to="/patient-dashboard/Harold/Hurst" />
+          )} />
           <Route path="/patient-dashboard/:firstName/:lastName" component={PatientDashboard} />
         </Switch>
       </div>
